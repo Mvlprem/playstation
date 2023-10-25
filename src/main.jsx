0a979e7394cf7175a6a3b8fa602e711dede0ebf7
Mvlprem <mvl.prem@outlook.com>
2023-10-25 18:52:17 +0530

    refactor styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Router from './router/Router'
-import GlobalStyles from './styles/GlobalStyles'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Router />
-    <GlobalStyles />
   </React.StrictMode>
 )
